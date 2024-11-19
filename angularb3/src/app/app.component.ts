04caae0939b89b02f92e54fa755582c44c5b7c9d
nicoHersant <nicolas.hersant@gmail.com>
2024-11-19 17:01:43 +0100

    mise en place d'un input bi-directionnel dnas salutation component

--- angularb3/src/app/app.component.ts
+++ angularb3/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SalutationComponent } from './component/salutation/salutation.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SalutationComponent],
+  imports: [RouterOutlet, SalutationComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
