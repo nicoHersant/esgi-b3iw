08b0c1fd817e1afc5c76fc5964b07b5574d11554
nicoHersant <nicolas.hersant@gmail.com>
2024-12-12 16:03:45 +0100

    correction ortho

--- angular/src/app/component/cart/cart.component.ts
+++ angular/src/app/component/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CartService } from '../../service/cart.service';
 import { IProduct } from '../../interface/product.interface';
 import { CurrencyPipe, NgFor, NgIf } from '@angular/common';
@@ -10,12 +10,16 @@ import { CurrencyPipe, NgFor, NgIf } from '@angular/common';
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
-export class CartComponent implements OnInit{
+export class CartComponent implements OnInit, OnChanges{
   cartService = inject(CartService);
   products: IProduct[] = [];
 
   ngOnInit(): void {
     this.products = this.cartService.getProducts();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    
   }
 
   get total(): number {
