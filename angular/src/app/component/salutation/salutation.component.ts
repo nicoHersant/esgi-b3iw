8ecc943150d7bfa25030e36e918649367e982c11
nicoHersant <nicolas.hersant@gmail.com>
2024-12-12 14:01:48 +0100

    start onchanges

--- angular/src/app/component/salutation/salutation.component.ts
+++ angular/src/app/component/salutation/salutation.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-salutation',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './salutation.component.html',
   styleUrl: './salutation.component.scss'
 })
-export class SalutationComponent {
-  message: string = 'Eductive Angular';
+export class SalutationComponent implements OnInit, OnChanges {
+  message: string = "empty";
+  count: number = 0;
+
+  ngOnInit(): void {
+    this.message = 'Eductive Angular';
+  }
+
+  ngOnChanges() {
+    this.count++;
+  }
 }
